4f52af507a0b7c3409ffc1683d4f905a422dc8fb
NewGen2022 <amirgimari@gmail.com>
2024-08-22 18:09:31 +0300

    connect to databse using sequelize package

---
+++ db/sequelize.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+// Create a new Sequelize instance
+const sequelize = new Sequelize(process.env.DB_CONNECTION, {
+    dialect: 'postgres',
+    logging: false,
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Database connection established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+module.exports = sequelize;
