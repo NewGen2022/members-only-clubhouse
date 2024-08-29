936eac0c1213c6ad0cf8d5069ba8e62a52632fe0
NewGen2022 <amirgimari@gmail.com>
2024-08-29 12:17:28 +0300

    added try catch block and if user exists - display it

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,42 @@
 const { validationResult } = require('express-validator');
-const { createUser } = require('../db/queries');
+const { getUserByUsername, createUser } = require('../db/queries');
+const e = require('express');
 
 const addUser = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).render('signup', { errors: errors.array() });
+        return res
+            .status(400)
+            .render('signup', { errors: errors.array(), prevData: req.body });
     }
 
-    const userData = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        username: req.body.username,
-        password: req.body.password1,
-    };
+    try {
+        const isUserExists = await getUserByUsername(req.body.username);
 
-    const newUser = await createUser(userData);
+        if (isUserExists) {
+            return res.status(400).render('signup', {
+                errors: [{ msg: 'Username already exists' }],
+                prevData: req.body,
+            });
+        }
 
-    res.status(201).redirect('/login');
+        const userData = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            username: req.body.username,
+            password: req.body.password1,
+        };
+
+        const newUser = await createUser(userData);
+
+        res.status(201).redirect('/login');
+    } catch (err) {
+        console.error('Error creating user:', err);
+        res.status(500).render('signup', {
+            errors: [{ msg: 'Internal server error, please try again later.' }],
+            prevData: req.body,
+        });
+    }
 };
 
 module.exports = { addUser };
