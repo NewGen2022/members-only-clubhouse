d0fd9936a44a651361646f19d054a43a4b655d06
NewGen2022 <amirgimari@gmail.com>
2024-08-29 11:30:20 +0300

    route for login and signup

---
+++ routes/authentication.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { addUser } = require('../controllers/authController');
+const { validateSignUp } = require('../middleware/validation.js');
+
+// GET routes
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// POST routes
+router.post('/signup', validateSignUp, addUser);
+
+module.exports = router;
