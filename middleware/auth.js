ccc176523da08d0dac1abf288f0c0f2db5918037
NewGen2022 <amirgimari@gmail.com>
2024-09-06 22:26:24 +0300

    removed else block (it do not work properly with it

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 const isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
-    } else {
-        res.status(401).render('error', { msg: 'NOT AUTHENTICATED' });
     }
+
+    res.status(401).render('error', { msg: 'NOT AUTHENTICATED' });
 };
 
-module.export = { isAuth };
+module.exports = { isAuth };
