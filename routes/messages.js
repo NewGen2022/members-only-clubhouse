23023e01f229488078236ac952d2af41736378e3
NewGen2022 <amirgimari@gmail.com>
2024-09-06 20:52:26 +0300

    moved function to controllers

--- routes/messages.js
+++ routes/messages.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMessages } = require('../db/queries');
-const { formatDate } = require('../public/js/utils');
+const { main } = require('../controllers/messagesController');
 
 // GET ROUTES
-router.get('/', async (req, res) => {
-    const isAuthenticated = req.isAuthenticated();
-    const messages = await getAllMessages();
-    res.render('index', {
-        isAuthenticated: isAuthenticated,
-        messages: messages,
-        user: req.user,
-        formatDate: formatDate,
-    });
-});
+router.get('/', main);
 
 module.exports = router;
