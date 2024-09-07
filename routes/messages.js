986300cc7456987186bb3a6967924641f8a8bcf1
NewGen2022 <amirgimari@gmail.com>
2024-09-07 16:19:47 +0300

    added post route for create-msg

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { main } = require('../controllers/messagesController');
+const { main, addMsg } = require('../controllers/messagesController');
 const { isAuth } = require('../middleware/auth');
 
 // GET ROUTES
@@ -9,5 +9,8 @@ router.get('/', main);
 router.get('/create-msg', isAuth, (req, res) => {
     res.render('create-msg', { user: req.user });
 });
+
+// POST ROUTES
+router.post('/create-msg', isAuth, addMsg);
 
 module.exports = router;
