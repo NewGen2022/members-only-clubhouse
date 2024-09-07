bf541d556a65292af5f34e3be81457eb9c89101a
NewGen2022 <amirgimari@gmail.com>
2024-09-07 18:09:30 +0300

    added routes for: get and post become-admin, get success message; added middleware for all get routes

--- routes/enhanceStatus.js
+++ routes/enhanceStatus.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const { isAuth } = require('../middleware/auth');
+const { isAuth, isMember, isAdmin } = require('../middleware/auth');
 const {
     updateUserToMember,
     updateUserToAdmin,
 } = require('../controllers/enhanceStatusController');
 
 // GET ROUTES
-router.get('/join-club', isAuth, (req, res) => {
+router.get('/join-club', isAuth, isMember, (req, res) => {
     res.render('./join_club/join-club', { user: req.user, errors: '' });
 });
 
-router.get('/join-club/success', isAuth, (req, res) => {
+router.get('/join-club/success', isAuth, isMember, (req, res) => {
     res.render('./join_club/success', { user: req.user });
 });
 
-router.get('/become-admin', isAuth, (req, res) => {
+router.get('/become-admin', isAuth, isAdmin, (req, res) => {
     res.render('./become_admin/become-admin', { user: req.user });
+});
+
+router.get('/become-admin/success', isAuth, isAdmin, (req, res) => {
+    res.render('./become_admin/success', { user: req.user });
 });
 
 // POST ROUTES
 router.post('/join-club', isAuth, updateUserToMember);
+
+router.post('/become-admin', isAuth, updateUserToAdmin);
 
 module.exports = router;
