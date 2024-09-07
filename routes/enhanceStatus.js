724549d31d40c0c6d670a38ba8e8320eca30ed76
NewGen2022 <amirgimari@gmail.com>
2024-09-07 17:07:46 +0300

    added routes for: get and post join-club, get success page when club joined

--- routes/enhanceStatus.js
+++ routes/enhanceStatus.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const { isAuth } = require('../middleware/auth');
+const {
+    updateUserToMember,
+    updateUserToAdmin,
+} = require('../controllers/enhanceStatusController');
 
+// GET ROUTES
 router.get('/join-club', isAuth, (req, res) => {
-    res.render('./join_club/join-club', { user: req.user });
+    res.render('./join_club/join-club', { user: req.user, errors: '' });
+});
+
+router.get('/join-club/success', isAuth, (req, res) => {
+    res.render('./join_club/success', { user: req.user });
 });
 
 router.get('/become-admin', isAuth, (req, res) => {
     res.render('./become_admin/become-admin', { user: req.user });
 });
+
+// POST ROUTES
+router.post('/join-club', isAuth, updateUserToMember);
 
 module.exports = router;
