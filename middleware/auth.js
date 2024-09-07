1fbed8b6ae496aef0f34428b32c5a83f60a5fe8f
NewGen2022 <amirgimari@gmail.com>
2024-09-07 12:45:56 +0300

    fixed error - just returning next()

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
-        next();
+        return next();
     }
 
     res.status(401).render('error', { msg: 'NOT AUTHENTICATED' });
