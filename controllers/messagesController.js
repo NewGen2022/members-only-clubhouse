8680226cb5f472cda448e7d673252ee89b71c866
NewGen2022 <amirgimari@gmail.com>
2024-09-09 20:43:15 +0300

    added controller for message deleting

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,4 +1,8 @@
-const { getAllMessages, createMessage } = require('../db/queries');
+const {
+    getAllMessages,
+    createMessage,
+    deleteMessageById,
+} = require('../db/queries');
 const { formatDate } = require('../public/js/utils');
 
 const main = async (req, res) => {
@@ -28,4 +32,18 @@ const addMsg = async (req, res) => {
     }
 };
 
-module.exports = { main, addMsg };
+const deleteMsg = async (req, res) => {
+    const messageId = req.body.messageId;
+
+    try {
+        await deleteMessageById(messageId);
+        return res.redirect('/');
+    } catch (error) {
+        console.error('Error deleting message:', error);
+        return res
+            .status(500)
+            .render('error', { msg: 'Internal Server Error' });
+    }
+};
+
+module.exports = { main, addMsg, deleteMsg };
